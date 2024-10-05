26d3b3731bcb55a455c1eccf4f27f017dac37659
AlvinoVC12 <alvinovc_12@hotmail.com>
2024-10-05 17:11:48 -0500

    primer commit

---
+++ src/main/java/com/cafeteria/SistemaDeGestionDeCafeteriaApplication.java
@@ -0,0 +1,13 @@
+package com.cafeteria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeGestionDeCafeteriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeGestionDeCafeteriaApplication.class, args);
+	}
+
+}
